d5010efd3cfb6f5267e8865a62b9392b1ddf9b51
Ankur Patel <32640688+ankurcloudworld@users.noreply.github.com>
2024-11-20 10:05:54 +0530

    Update main.tf

--- storageacc/main.tf
+++ storageacc/main.tf
@@ -15,8 +15,6 @@ module "avm-res-storageaccount" {
   name                = "${var.storage_account_name}${var.environment_name}"
   account_tier        = "Standard"
   account_kind        = "StorageV2"
-  https_traffic_only_enabled = true
-  public_network_access_enabled = true
   shared_access_key_enabled = false
   depends_on          = [module.resource_group]
 }
