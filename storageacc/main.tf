2eec8077afef618b9497316e7cf8dfc05c0babc7
Ankur Patel <32640688+ankurcloudworld@users.noreply.github.com>
2024-11-25 09:40:23 +0530

    Update main.tf

--- storageacc/main.tf
+++ storageacc/main.tf
@@ -1,28 +1,20 @@
-# Define local values with the correct block name "locals"
-locals {
-  resource_group_name = length(data.azurerm_resource_group.existing_rg.*.id) > 0 ? 
-    data.azurerm_resource_group.existing_rg.name : 
-    azurerm_resource_group.new_rg[0].name
-}
-
-# Data block to fetch existing resource group
+# Data block to check for an existing resource group
 data "azurerm_resource_group" "existing_rg" {
-  name     = var.resource_group_name
-  provider = azurerm
+  name = var.resource_group_name
 }
 
-# Create resource group only if it doesn't exist
+# Resource block to create the resource group if it doesn't exist
 resource "azurerm_resource_group" "new_rg" {
-  count = length(data.azurerm_resource_group.existing_rg.*.id) > 0 ? 0 : 1
+  count = data.azurerm_resource_group.existing_rg.id != "" ? 0 : 1
 
   name     = var.resource_group_name
   location = var.location
 }
 
-# Use the appropriate resource group for the storage account
+# Use the appropriate resource group in the storage account
 resource "azurerm_storage_account" "sa" {
   name                     = "${var.storage_account_name}${var.environment_name}"
-  resource_group_name      = local.resource_group_name
+  resource_group_name      = coalesce(data.azurerm_resource_group.existing_rg.name, azurerm_resource_group.new_rg[0].name)
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
