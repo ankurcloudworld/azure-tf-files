b3b2b6a2a04f66b5b3fc83d7683fe4cf1fa6c932
Ankur Patel <32640688+ankurcloudworld@users.noreply.github.com>
2024-11-25 09:34:23 +0530

    Update main.tf

--- storageacc/main.tf
+++ storageacc/main.tf
@@ -1,23 +1,25 @@
-# Fetch the resource group if it already exists
-data "azurerm_resource_group" "existing_rg" {
-  name     = var.resource_group_name
+# Define local values with the correct block name "locals"
+locals {
+  resource_group_name = length(data.azurerm_resource_group.existing_rg.*.id) > 0 ? 
+    data.azurerm_resource_group.existing_rg.name : 
+    azurerm_resource_group.new_rg[0].name
 }
 
-# Create the resource group only if it doesn't exist
+# Data block to fetch existing resource group
+data "azurerm_resource_group" "existing_rg" {
+  name     = var.resource_group_name
+  provider = azurerm
+}
+
+# Create resource group only if it doesn't exist
 resource "azurerm_resource_group" "new_rg" {
-  count = length(data.azurerm_resource_group.existing_rg.id) > 0 ? 0 : 1
+  count = length(data.azurerm_resource_group.existing_rg.*.id) > 0 ? 0 : 1
 
   name     = var.resource_group_name
   location = var.location
 }
 
-# Use the appropriate resource group
-local {
-  resource_group_name = length(data.azurerm_resource_group.existing_rg.id) > 0 ? 
-    data.azurerm_resource_group.existing_rg.name : 
-    azurerm_resource_group.new_rg[0].name
-}
-
+# Use the appropriate resource group for the storage account
 resource "azurerm_storage_account" "sa" {
   name                     = "${var.storage_account_name}${var.environment_name}"
   resource_group_name      = local.resource_group_name
